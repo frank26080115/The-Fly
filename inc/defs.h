e67ebbc99659103a678e83094cbcdc39c0b91c2e
Frank Zhao <frank.zhao.main@gmail.com>
2026-05-14 13:16:47 -0700

    no debugging fields in file packet header

--- inc/defs.h
+++ inc/defs.h
@@ -21,14 +21,12 @@ enum
 // this structure is a fixed size, which makes everything less complicated, and enables easier scrubbing
 typedef struct __attribute__((packed))
 {
-    uint32_t magic;  // always FILE_PACKET_HEADER_MAGIC, makes it easier to sync
-    uint8_t src:4;   // indicates where the audio came from, AUDSRC_BT or AUDSRC_MIC
-    uint8_t flags:4; // indicates any warnings from the FIFO
-    #ifdef FILE_CONTAINS_DEBUG
-    uint32_t ms_timestamp; // millis() when written to file
-    uint32_t sequence_num;
-    uint32_t fifo_cnt;     // count before dequeue
-    #endif
+    uint32_t magic;          // always FILE_PACKET_HEADER_MAGIC, makes it easier to sync
+    uint8_t  src;            // indicates where the audio came from, AUDSRC_BT or AUDSRC_MIC
+    uint8_t  flags;          // indicates any warnings from the FIFO
+    uint32_t ms_timestamp;   // millis() when written to file
+    uint32_t sequence_num;   // per whole file, not per channel/FIFO
+    uint32_t fifo_cnt;       // count before dequeue
     uint16_t payload_length; // length of payload, up to the max allowed
     uint16_t payload[FILE_PACKET_PAYLOAD_MAX];
 }
