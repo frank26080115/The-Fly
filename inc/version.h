facc90b212dbf9002ba65fb4026b710f84bb187e
Frank Zhao <frank.zhao.main@gmail.com>
2026-05-15 11:56:26 -0700

    compile version and time strings

---
+++ inc/version.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+extern const char* version_str;
+extern const char* compiler_version_str;
+extern const char* compiler_time_str;
+
+#ifdef __cplusplus
+}
+#endif
