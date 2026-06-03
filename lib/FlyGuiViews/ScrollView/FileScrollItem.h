6508f6595eb442daba8ba77aca0531ff1f75eab5
Frank Zhao <frank.zhao.main@gmail.com>
2026-06-03 11:37:57 -0700

    file size/duration in scrollview

--- lib/FlyGuiViews/ScrollView/FileScrollItem.h
+++ lib/FlyGuiViews/ScrollView/FileScrollItem.h
@@ -8,13 +8,25 @@ class FileScrollItem : public ScrollItem
 public:
     FileScrollItem();
 
-    void configureFile(ScrollItemKind kind, int32_t callbackValue, const char* fileName);
+    void configureFile(ScrollItemKind kind, int32_t callbackValue, const char* fileName, uint64_t fileSize);
 
     uint32_t fileHash() const
     {
         return fileHash_;
     }
 
+    uint64_t fileSize() const
+    {
+        return fileSize_;
+    }
+
+    const char* detailText() const override
+    {
+        return detailText_;
+    }
+
 private:
-    uint32_t fileHash_ = 0;
+    uint32_t fileHash_       = 0;
+    uint64_t fileSize_       = 0;
+    char     detailText_[32] = {};
 };
